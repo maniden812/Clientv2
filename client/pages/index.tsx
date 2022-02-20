5733c92f0e7f277e02435fa8f2897275d6405706
ana530 <chetanapitani@gmail.com>
2022-02-20 15:52:40 -0600

    updated nav bar, created home page - chetana

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { Router, useRouter } from 'next/router'
 import {Navbar} from '../components/Navbar/Navbar'
-
+import styles from './index.module.css'
 
 const Home: NextPage = () => {
   const {data: session} = useSession()
@@ -16,19 +16,13 @@ const Home: NextPage = () => {
       <div>
         <Navbar />
         <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-
-        <Head>
-          <div>Hello World</div>
-        
-        </Head>
+        <br/>
+        <br/>
+        <br/>
+        <div>
+          <h1 className = {styles.bigtext}>Track. Share. Save.</h1>
+          <h1 className = { styles.smalltext }>Welcome to FuelQuote. Our platform is here to serve you better and faster. We will provide you the best prices and customer service. Created by Kevin Flores, Manasa Dendukuri, Chetana Pitani</h1>
+        </div>
 
       </div>
       
